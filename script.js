790095ab7b1f22e0f2db2ce1dd6d398de0608728
WijayaAdam <143468234+WijayaAdam@users.noreply.github.com>
2023-09-14 08:56:43 +0700

    JS DOM Part1

---
+++ script.js
@@ -0,0 +1,29 @@
+const judul = document.getElementById('judul');
+judul.style.color='white';
+judul.style.backgroundColor = 'blue';
+judul.innerHTML='Ryuuga Editz';
+
+//const p = document.getElementsByTagName('p');
+//p[0].style.backgroundColor='red';
+//p[1].style.backgroundColor='yellow';
+//p[2].style.backgroundColor='green';
+//p[3].style.backgroundColor='purple';
+
+const h1 = document.getElementsByTagName('h1');
+h1[0].style.fontSize='50px';
+
+//const p1 = document.getElementsByClassName('p1');
+//p1[0].innerHTML= "dirubah dengan javascript";
+
+const p4 = document.querySelector('#b p');
+p4.style.color = 'green';
+p4.style.fontSize = '25px';
+
+const li2 = document.querySelector('li:nth-child(2)');
+li2.style.backgroundColor = 'lightcoral';
+
+const p1 = document.querySelector('p');
+p1.innerHTML = 'ini dirubah menggunakan javascript';
+
+const p = document.querySelectorAll('p');
+p[2].style.backgroundColor='lightblue';
